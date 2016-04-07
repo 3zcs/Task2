be4d2b76fb3eabcdc0db77d2fb303a25577b52b6
*3zcs* <*abdulaziz.aloshin@gmail.com*>
2016-04-07 07:51:10 +0300

    final project

--- app/src/main/java/com/example/azcs/task1/MapsActivity.java
+++ app/src/main/java/com/example/azcs/task1/MapsActivity.java
@@ -1,32 +1,66 @@
 package com.example.azcs.task1;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap , mMaps;
-    private static final LatLng Saud = new LatLng(24.722575, 46.627601);
-    //private static final LatLng Nourah= new LatLng(24.852394, 46.723890);
+import java.io.IOException;
+import java.util.List;
 
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener{
+    private GoogleMap mMap, mMapUser;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        ActionBar actionBar = getSupportActionBar();
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
     }
 
 
@@ -42,26 +76,118 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMaps = googleMap;
+        mMapUser = googleMap;
+
+        //to set blue point on my current location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        //enable zoom in zoom out
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        /*add and control marker
+            - one marker in map , remove old one if you make new
+            - marker title show you the full details about location
+            - you can't set a marker with zoom bigger then 15 of the message will printed
+         */
+        mMapUser.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                int x = (int) mMapUser.getCameraPosition().zoom;
+                if (x >= 15) {
+                    mMapUser.clear();
+                    Marker a = mMapUser.addMarker(new MarkerOptions().
+                            position(latLng).
+                            icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    List<Address> addresses = null;
+                    try {
+                        addresses = geocoder.getFromLocation(a.getPosition().latitude, a.getPosition().longitude, 1);
+                        String address = addresses.get(0).getAddressLine(0);
+                        String city = addresses.get(0).getAddressLine(1);
+                        String country = addresses.get(0).getAddressLine(2);
+                        a.setTitle(address + " " + city + " " + country);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }else{
+                Toast.makeText(getApplicationContext() , R.string.attention , Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
+
+    //get two icons from menu res
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //change the map type based on your icon select
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.milayers :
+                // Sets the map type to be "hybrid"
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case R.id.mimap :
+                // Sets the map type to be "normal"
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
 
-        // Add a marker in King Saud University
-        mMap.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
-                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .title("King Saud University")
-                .position(new LatLng(24.722575, 46.627601)));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        // Add a marker in Princess Nourah Bint Abdulrahman University
-        mMap.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
-                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .title("Princess Nourah Bint Abdulrahman University")
-                .position(new LatLng(24.852394, 46.723890)));
+    //to move the camera to my current location with 13 zoom when the app start
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 13);
+        mMap.animateCamera(cameraUpdate);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(this);
+    }
 
-        // Move the camera instantly to King Saud University with a zoom of 11
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Saud, 11));
 
+    //I put the next methods to avoid errors don't ask me why
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
 }
